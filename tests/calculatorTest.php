8c95925855df6252c0f86e8eafa27571b608994a
Asim Bashir <asim.bashir@rolustech.net>
2024-08-28 15:31:25 +0500

    composer json updated and added php unit test cases

---
+++ tests/calculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(5, $calculator->add(2, 3));
+        $this->assertEquals(0, $calculator->add(-2, 2));
+    }
+}
