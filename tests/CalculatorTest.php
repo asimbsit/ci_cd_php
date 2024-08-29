fd4c88f142afa538c0a07629f1066ccf90a8c05c
asimbsit <asimbsit@gmail.com>
2024-08-29 16:55:17 +0500

    checking failed test case

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -11,6 +11,6 @@ class CalculatorTest extends TestCase
     {
         $calculator = new Calculator();
         $this->assertEquals(5, $calculator->add(2, 3));
-        $this->assertEquals(0, $calculator->add(-2, 2));
+        $this->assertEquals(1, $calculator->add(-2, 2));
     }
 }
